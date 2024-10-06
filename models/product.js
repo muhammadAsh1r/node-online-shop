e1d08f577e2373cc835dcab60f45dbee4a411c0b
Muhammad Ashir Khan <just4fun17678@gmail.com>
2024-10-05 20:12:42 -0700

    Storing Product Data

--- models/product.js
+++ models/product.js
@@ -2,8 +2,11 @@ const fs = require("fs");
 const path = require("path");
 
 module.exports = class Product {
-  constructor(title) {
+  constructor(title, imageUrl, description, price) {
     this.title = title;
+    this.imageUrl = imageUrl;
+    this.description = description;
+    this.price = price;
   }
 
   save() {
