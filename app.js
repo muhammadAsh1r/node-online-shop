b67c6bcd069247038cfe690c8f451e3361942614
Muhammad Ashir Khan <just4fun17678@gmail.com>
2024-10-05 18:51:32 -0700

    Uploading basic store app

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const expressHbs = require("express-handlebars");
+
+const path = require("path");
+
+const app = express();
+
+const errorController = require('./controllers/error')
+
+app.set('view engine', 'ejs');
+app.set("views", "./views");
+
+const adminRoutes = require("./routes/admin");
+const shopRoutes = require("./routes/shop");
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use("/admin", adminRoutes);  
+app.use(shopRoutes);
+
+app.use(errorController.get404);
+
+app.listen(3000);
